7a6ebbd5eb4f69ed6ad05c845217ddcd4ee50bc6
xoobeing <ahnsb0418@snu.ac.kr>
2025-06-23 11:39:03 +0900

    sweetalert2 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+import Swal from 'sweetalert2';
+
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('dangerButton');
   button.addEventListener('click', () => {
-    alert('클릭하지 마세요!');
+    Swal.fire({
+      title: '경고!',
+      text: '클릭하지 말라고 했잖아요!',
+      icon: 'warning',
+      confirmButtonText: '죄송합니다...',
+      background: '#fff0f5', // 파스텔 배경
+      confirmButtonColor: '#ff99aa',
+    });
   });
 });
