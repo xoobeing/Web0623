618e827547e1ef578f2440edd383e0af6d7f2e2e
xoobeing <ahnsb0418@snu.ac.kr>
2025-06-23 11:17:09 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('dangerButton');
+  button.addEventListener('click', () => {
+    alert('클릭하지 마세요!');
+  });
+});
